975748e5bcaeec30530f7100ebb0cb69e98074e6
SpunBeat <olvera.alan@gmail.com>
2016-06-15 13:51:30 -0500

    Integracion

--- public/app/modules/areas/controllers/gobierno.controller.js
+++ public/app/modules/areas/controllers/gobierno.controller.js
@@ -1,8 +1,11 @@
 /*global angular*/
 
 (function () {
-    var Controller = function() {
-        
+    var Controller = function($scope) {
+        $scope.go = function() {
+            window.location = '#/areas/arquitectura/areas';  
+        };
     };
+    Controller.$inject = ['$scope'];
     angular.module('mAreas').controller('GobiernoController', Controller);
 })();
