6028b63d24c7b606e6255086ca33cfc95863142a
SpunBeat <olvera.alan@gmail.com>
2016-06-21 18:59:23 -0500

    Cambios de ruta service

--- public/app/config/app.service.js
+++ public/app/config/app.service.js
@@ -2,8 +2,8 @@
 
 (function() {
     angular.module('mainService', []).factory('$itbook', function($resource) {
-//       return $resource('http://localhost:5000/homs.json');
+       return $resource('http://localhost:5000/homs.json');
 //        return $resource('http://10.203.25.253:8080/ITBook/Procesos');
-        return $resource('http://localhost:8080/ITBook/Procesos');
+//        return $resource('http://localhost:8080/ITBook/Procesos');
     });
 })();
