e5456856aa4ed41760047d2f446e988f78618c62
SpunBeat <olvera.alan@gmail.com>
2016-06-24 11:47:30 -0500

    Se agrega el controlador de areas

--- public/app/modules/areas/controllers/arq-areas.controller.js
+++ public/app/modules/areas/controllers/arq-areas.controller.js
@@ -1,11 +1,76 @@
 /*global angular*/
 
 (function () {
-    var Controller = function($scope) {
+    var Controller = function($scope, $window) {
+        
         $scope.go = function() {
-            window.location = '#/areas/arquitectura/gobierno';  
+            $window.location = '#/areas/arquitectura/gobierno';  
         };
+        
+        $scope.areas = [
+            {
+                nombre: 'Arquitectura de Negocio',
+                imagen: '',
+                descripcion: '',
+                subdirector: {
+                    nombre: '',
+                    puesto: '',
+                    trayectoria: ''
+                }
+            },
+            {
+                nombre: 'Arquitectura de Datos y Aplicaciones',
+                imagen: '',
+                descripcion: '',
+                subdirector: {
+                    nombre: '',
+                    puesto: '',
+                    trayectoria: ''
+                }
+            },
+            {
+                nombre: 'Arquitectura de Tecnología',
+                imagen: '',
+                descripcion: '',
+                subdirector: {
+                    nombre: '',
+                    puesto: '',
+                    trayectoria: ''
+                }
+            },
+            {
+                nombre: 'Análisis Estratégico',
+                imagen: '',
+                descripcion: '',
+                subdirector: {
+                    nombre: '',
+                    puesto: '',
+                    trayectoria: ''
+                }
+            },
+            {
+                nombre: 'Gobierno de TI',
+                imagen: '',
+                descripcion: '',
+                subdirector: {
+                    nombre: '',
+                    puesto: '',
+                    trayectoria: ''
+                }
+            },
+            {
+                nombre: 'Experiencia Digital',
+                imagen: '',
+                descripcion: '',
+                subdirector: {
+                    nombre: '',
+                    puesto: '',
+                    trayectoria: ''
+                }
+            },
+        ];
+        
     };
-    Controller.$inject = ['$scope'];
+    Controller.$inject = ['$scope', '$window'];
     angular.module('mAreas').controller('ArqAreasController', Controller);
 })();
