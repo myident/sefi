5db1fd47160e3205cf7869afa516bd1cb6afc421
SpunBeat <olvera.alan@gmail.com>
2016-06-29 13:43:08 -0500

    Piraña

--- public/app/config/app.service.js
+++ public/app/config/app.service.js
@@ -3,8 +3,8 @@
 (function() {
     angular.module('mainService', []).factory('$itbook', function($resource, $singleton) {
         var path = $singleton.path;
-//       return $resource('http://localhost:5000/homs.json');
-        return $resource(path + '/ITBook/Procesos');
+       return $resource('http://localhost:5000/homs.json');
+//        return $resource(path + '/ITBook/Procesos');
 //        return $resource('http://localhost:8080/ITBook/Procesos');
     });
 })();
