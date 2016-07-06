aaead18121e9ad5c54194783b0e07bb51dc51705
Martín Mtz <martin.ml@icloud.com>
2016-07-06 11:40:14 -0500

    paragraph

---
+++ public/app/shared/directives/main/ng-paragraph/ng-paragraph.directive.js
@@ -0,0 +1,35 @@
+/* global angular */
+
+(function () {
+    var Directive = function () {
+        var Link = function ($scope) {
+            console.log($scope.text);
+            $scope.arr = [];
+            var count = 0;
+            for(var i = 0; i < $scope.text.length; i++){
+                // console.log($scope.text[i]);
+                
+                if($scope.text[i] + $scope.text[i+1] === '\\n'){
+                    count++;
+                    i += 1;
+                    console.log(count);
+                    $scope.arr[count] = '';
+                }else{
+                    $scope.arr[count] += $scope.text[i];
+                }
+            }
+            console.log($scope.arr);
+        };
+        return {
+            restrict: 'E',
+            templateUrl: 'app/shared/directives/main/ng-paragraph/ng-paragraph.template.html',
+            link: Link,
+            scope: {
+                text:"=text"
+            }
+        };
+    };
+    angular
+        .module('mParagraph', [])
+        .directive('ngParagraph', Directive);
+})();
