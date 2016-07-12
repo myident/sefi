5e19dc3bb6bc7f5cc7fb661d139bcec8a1380323
SpunBeat <olvera.alan@gmail.com>
2016-07-12 11:03:07 -0500

    Servicio Print

--- public/app/config/app.module.js
+++ public/app/config/app.module.js
@@ -4,7 +4,7 @@
     var angularModules      = ["ngRoute","ngTouch", "ngResource", "ngAnimate"];
     var customModules       = ["mHome", "mProcesses", "mEcosistema", "mAreas", "mBusinessArchitecture","mAdmin"];
     var componentsModules   = ["mHeaderDirective", "mBackground", "Spin","mSlideview","mParagraph"];
-    var services            = ["mainService", "mVash", "Singleton", "Arquitecturas", "Dominios", "Megaprocesos", "Macroprocesos", "Procesos","Arquitecturaseco"];
+    var services            = ["mainService", "mVash", "Singleton", "Arquitecturas", "Dominios", "Megaprocesos", "Macroprocesos", "Procesos","Arquitecturaseco", "mPrint"];
 
     var modules = angularModules.concat(customModules,componentsModules, services);
     
