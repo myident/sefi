e9be9b5a3149b96edeedcaf7980f7f09bb4abfc0
Martín Mtz <martin.ml@icloud.com>
2016-07-14 13:19:44 -0500

    >.<

--- public/app/modules/home/controllers/home.controller.js
+++ public/app/modules/home/controllers/home.controller.js
@@ -14,13 +14,13 @@
         $scope.home = {
             "article":{
                     "title":"Hello!",
-                    "body":"I am proud to introduce to all IT Collaborators and Business Users areas this new tool, which has been redesigned to consult, save and update, in an easier way, the IT documentation that  is generated during planning, design and development of the requirements requested to IT from the Business Areas of AT&T Mexico. \\n \\n This tool will provide you an overview of our Enterprise Architecture, and its main objective is to keep the information allways available and updated. Everyone is invited to be part of its ongoing improvement.",
+                    "body":"I am proud to introduce to all IT Collaborators and Business Users areas this new tool, which has been redesigned to consult, save and update, in an easier way, the IT documentation that  is generated during planning, design and development of the requirements requested to IT from the Business Areas of AT&T Mexico. \\n This tool will provide you an overview of our Enterprise Architecture, and its main objective is to keep the information allways available and updated. Everyone is invited to be part of its ongoing improvement.",
                     "author":"Pablo Salazar",
                     "job":"VP IT / Chief Information Officer"
                     },
             "vision":"To design and create in this decade the new global network, processes, and service platforms that maximize automation, allowing for a reallocation of human resources to more complex and productive work.",
             "mission":"Our mission is to exploit technical innovations for the benefit of AT&T and its customers by implementing next-generation technologies and network advancements in AT&T's services and operations.",
-            "values":["Ethics","Ethics", "Honor your Word","Honesty","Respect","Family","Discipline"],
+            "values":["Ethics", "Honor your Word","Honesty","Respect","Family","Discipline"],
             "other":" “Connecting \\n people, \\n processes \\n​ and things.” "
         };
         
@@ -29,3 +29,4 @@
     angular.module('mHome').controller('HomeController', controller);
 
 })();
+
