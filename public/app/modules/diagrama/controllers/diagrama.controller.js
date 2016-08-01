2edaaa6b63c51cc6f2f4976eca5aab46b6768b06
SpunBeat <olvera.alan@gmail.com>
2016-08-01 18:59:56 -0500

    Integración de la directiva dgIndice

--- public/app/modules/diagrama/controllers/diagrama.controller.js
+++ public/app/modules/diagrama/controllers/diagrama.controller.js
@@ -1,11 +1,79 @@
 /*global angular*/
 
 (function () {
-    var Controller = function ($scope, $rootScope) {
+    var Controller = function ($scope, $rootScope, $window, $arquitecturas, $megaprocesos, $macroprocesos, $procesos) {
+
         $rootScope.spin = false;
+
+        $scope.arquitecturas = {};
+        $scope.arquitectura = {};
+        $scope.dominios = [];
+        $scope.megaprocesos = [];
+        $scope.macroprocesos = [];
+        $scope.procesos = [];
+
+        $scope.arquitecturaElegida = 0;
+        $scope.dominioElegido = 0;
+
+        // webservice dominios
+        $scope.arquitecturas = $arquitecturas.get(function (data) {
+            $scope.arquitectura = data.arquitectura[$scope.arquitecturaElegida];
+            if (data.arquitectura[$scope.arquitecturaElegida].dominios.length) {
+                // Fill Dominios
+                $scope.dominios = data.arquitectura[$scope.arquitecturaElegida].dominios;
+                $scope.dominios[$scope.dominioElegido].open = true;
+                // Fill megaprocesos
+                $scope.megaprocesos = $megaprocesos.query({
+                    iddominio: $scope.dominios[$scope.dominioElegido].id
+                }, function(){
+                    
+                }, function(e){
+                   console.log(e); 
+                });
+            }
+        }, function (e) {
+            console.log(e);
+        });
+
+        // webservice dominios //*** se activa desde la directiva
+        $scope.dominioIndex = function (value) {
+            $scope.megaprocesos = $megaprocesos.query({
+                iddominio: value
+            }, function(){
+                
+            }, function(e){
+                console.log(e);
+            });
+        };
+
+        // webservice macroprocesos //*** se activa desde la directiva
+        $scope.megaprocesoIndex = function (value, index) {
+            $scope.macroprocesos = $macroprocesos.query({
+                idmegaproceso: value
+            }, function (data) {
+                $scope.megaprocesos[index].macroprocesos = data;
+            }, function (e) {
+                console.log(e);
+            });
+        };
+
+        // webservice procesos //*** se activa desde la directiva
+        $scope.macroprocesoIndex = function (value) {
+            $procesos.get({
+                idmacroproceso: value
+            }, function (data) {
+                $scope.procesos = data.procesos;
+            }, function (e) {
+                console.log(e);
+            });
+        };
+
+        $scope.regresar = function () {
+            $window.history.back();
+        };
     };
 
-    Controller.$inject = ['$scope', '$rootScope'];
+    Controller.$inject = ['$scope', '$rootScope', '$window', '$arquitecturas', '$megaprocesos', '$macroprocesos', '$procesos'];
 
     angular
         .module('mDiagrama').controller('DiagramaController', Controller);
